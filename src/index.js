bc8ee33030a902d81d41e72bef44f966cb5bf682
Maria <mariashklovskaya@coherentsolutions.com>
2018-02-22 09:52:36 +0300

    rolling-scope/feature: brace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let charArr = str.split('');
+  let stack = [];
+  let debug = false;
+
+  charArr.forEach((char, i) => {
+    let openIndex = bracketsConfig.findIndex(item => item[0] === char);
+    let closeIndex = bracketsConfig.findIndex(item => item[1] === char);
+    
+    if(openIndex >= 0 && closeIndex >= 0) {
+      (stack.includes(openIndex)) ? openIndex = -1 : closeIndex = -1;
+    }
+
+    if(openIndex >= 0) {
+      stack.push(openIndex);
+    } else if (closeIndex >= 0 && closeIndex === stack[stack.length-1]) {
+      stack.pop();
+    } else {
+      return;
+    }
+  });
+  return (stack.length) ?  false : true;
 }
